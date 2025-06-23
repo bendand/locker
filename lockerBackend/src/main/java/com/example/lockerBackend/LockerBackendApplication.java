7c6452cc24028e43d5aede2cbd6b5dec725af85f
Ben <bendanderson13@gmail.com>
2025-06-22 22:45:28 -0500

    testing locker backend config

---
+++ lockerBackend/src/main/java/com/example/lockerBackend/LockerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerBackendApplication.class, args);
+	}
+
+}
---
+++ lockerBackend/src/test/java/com/example/lockerBackend/LockerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lockerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
